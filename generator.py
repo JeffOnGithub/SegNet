8ee64aaa7dcbb5fdc9fe6f37e15a400aff6cd6e4
Jeff Grenier <jeff@grenier.cc>
2018-05-02 18:28:14 -0400

    First commit
    Doing some cleanup

--- generator.py
+++ generator.py
@@ -2,10 +2,9 @@
 import numpy as np
 import cv2
 from keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
-from scipy.misc import imresize
 
 
-def catelab(labels, dims, n_labels):
+def to_categorical_labels(labels, dims, n_labels):
     x = np.zeros([dims[0], dims[1], n_labels])
     for i in range(dims[0]):
         for j in range(dims[1]):
@@ -29,7 +28,7 @@ def data_gen_small(img_dir, mask_dir, lists, batch_size, dims, n_labels):
                 # masks
                 original_mask = cv2.imread(mask_dir + lists.iloc[i, 0] + '.png')
                 resized_mask = cv2.resize(original_mask, (dims[0], dims[1]))
-                array_mask = binarylab(resized_mask[:, :, 0], dims, n_labels)
+                array_mask = to_categorical_labels(resized_mask[:, :, 0], dims, n_labels)
                 labels.append(array_mask)
             imgs = np.array(imgs)
             labels = np.array(labels)
